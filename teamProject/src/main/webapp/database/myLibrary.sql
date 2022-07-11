44cddf05958e5ec1bccc179e18aac055ab2958c5
Administrator <Administrator@322-25>
2022-07-11 15:08:48 +0900

    DB

--- teamProject/src/main/webapp/database/myLibrary.sql
+++ teamProject/src/main/webapp/database/myLibrary.sql
@@ -5,17 +5,16 @@ use book;
 create table myLibrary(
 	title varchar(500) not null,
     thumbnail varchar(500) not null,
-    isbn varchar(20) primary key,
+    isbn varchar(20),
     id int,
     authors varchar(20) not null,
     contents varchar(2000) not null,
     modifiedAt datetime,
-	foreign key (title) references dibsBook(title),
-    foreign key (thumbnail) references dibsBook(thumbnail),
     foreign key (isbn) references dibsBook(isbn),
-    foreign key (id) references users(id),
-    foreign key (authors) references dibsBook(authors)    
+    foreign key (id) references users(id)
 );
+
+insert into myLibrary values('°­¾ÆÁö¶Ë','12345','0001',1,'±è³ª¿¬','°­¾ÆÁö´Â ±Í¿±´Ù.',"2022-07-11");
 
 
 select * from myLibrary;
