1b650a596ed5cd0dab0fa61dd2aea6fc747b5667
Administrator <Administrator@322-25>
2022-07-11 12:11:07 +0900

    userDB

--- teamProject/src/main/webapp/database/user.sql
+++ teamProject/src/main/webapp/database/user.sql
@@ -5,8 +5,7 @@ create table user(
 	id varchar(20) unique not null,
     pw varchar(20) not null,
     `name` varchar(20) not null,
-    usercode smallint(8) primary key,
-    `point` int default 1000,
+    usercode int primary key,
     address varchar(500)
 );
 
