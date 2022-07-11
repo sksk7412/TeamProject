235f9846e6372abef223bfaadf7dbff78655fedc
Administrator <Administrator@322-25>
2022-07-11 15:14:08 +0900

    db

--- teamProject/src/main/webapp/database/dibsBook.sql
+++ teamProject/src/main/webapp/database/dibsBook.sql
@@ -1,14 +1,17 @@
 create database book;
 
 use book;
+
 create table dibsBook(
 	title varchar(500) not null,
     thumbnail varchar(500) not null,
     isbn varchar(20) primary key,
-    usercode int,
+    id int,
     authors varchar(20) not null,
     createAt datetime,
-    foreign key (usercode) references user(usercode)
-); 
+    foreign key (id) references users(id)
+);
+
+insert into dibsBook values('°­¾ÆÁö¶Ë','12345','0001',1,'±è³ª¿¬',"2022-07-11");
 
 select * from dibsBook;
--- teamProject/src/main/webapp/database/users.sql
+++ teamProject/src/main/webapp/database/users.sql
@@ -1,6 +1,7 @@
 create database book;
 
 use book;
+
 create table users(
     id int primary key,
 	userId varchar(20) unique not null,
@@ -8,5 +9,7 @@ create table users(
     `name` varchar(20) not null,
     mobile varchar(13) not null
 );
+
+insert into users(userId,userPw,`name`,mobile) values('a','1111','°¡³ª´Ù','010-1111-1111');
 
 select * from users;
