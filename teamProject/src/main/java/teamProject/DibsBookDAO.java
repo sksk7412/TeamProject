ead3e05c037ba8d33da56464a7e89bb3d01eab57
Administrator <Administrator@322-26>
2022-07-11 18:09:09 +0900

    commit

--- teamProject/src/main/java/teamProject/DibsBookDAO.java
+++ teamProject/src/main/java/teamProject/DibsBookDAO.java
@@ -26,7 +26,7 @@ public class DibsBookDAO {
 	private PreparedStatement pstmt = null;
 	private String log;
 	private String url = "jdbc:mysql://localhost:3306/";
-	private String database = "userdata";
+	private String database = "book";
 	private String user = "root";
 	private String password = "root";
 	Random ran = new Random();
@@ -42,16 +42,15 @@ public class DibsBookDAO {
 			System.out.println("code: "+BoardDto.getId());
 			pstmt = conn.prepareStatement(SQL);
 			
-			pstmt.setInt(1, BoardDto.getNo());
-			pstmt.setInt(2, BoardDto.getCode());
-			pstmt.setString(3, BoardDto.getTitle());
-			pstmt.setString(4, BoardDto.getContents());
-			pstmt.setInt(5, BoardDto.getViewCnt());
-			pstmt.setInt(6, BoardDto.getLikeCnt());
+			pstmt.setString(1, BoardDto.getTitle());
+			pstmt.setString(2, BoardDto.getThumbnail());
+			pstmt.setString(3, BoardDto.getIsbn());
+			pstmt.setInt(4, log);
+			pstmt.setString(5, BoardDto.getAuthors());
 			Timestamp createAt = new Timestamp(System.currentTimeMillis());
 			//pstmt.setString(4, createAt.toString());
-			pstmt.setTimestamp(7, createAt);
-			pstmt.setTimestamp(8, null);
+			pstmt.setTimestamp(6, createAt);
+
 				
 			return pstmt.executeUpdate();
 			
@@ -61,6 +60,40 @@ public class DibsBookDAO {
 		return -1;
 	}
 
-
+	// DB에서 값 불러오기
+		public Map getMap() {
+			Map<Integer, DibsBookDTO> bookData = new HashMap<>();
+			
+			conn = DBmanager.getConnection("book");
+			String sql = "select * from dibsBook";
+			pstmt = null;
+			
+			try {
+				pstmt = conn.prepareStatement(sql);
+				rs = pstmt.executeQuery();
+				
+				int id;
+				String title, thumbnail, isbn, authors;
+				Timestamp createAt;
+				
+				while(rs.next()) {
+					title = rs.getString(1);
+					thumbnail = rs.getString(2);
+					isbn = rs.getString(3);
+					id = rs.getInt(4);
+					authors = rs.getString(5);
+					createAt = rs.getTimestamp(6);
+					
+					DibsBookDTO dibs = new DibsBookDTO(id, title, thumbnail, isbn, authors, createAt);
+					
+					bookData.put(id, dibs);
+				}
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			return bookData;
+		}
 
 }
--- teamProject/src/main/webapp/_0.detail.jsp
+++ teamProject/src/main/webapp/_0.detail.jsp
@@ -7,20 +7,29 @@
 <title>Insert title here</title>
 </head>
 <body>
-	
-	<script src ="bookApi.js">
-		function getInfo(book){
-			String title = book.title();
-			String thumbnail = book.thumbnail();
-			String isbn =book.isbn();
-			int id = book.id();
-			String authors = book.authors();
-			Timestamp createdAt = 
-			
-		}
+
+	<%
+		String url = request.getParameter("url");
+		String title = request.getParameter("title");
+		String contents = request.getParameter("contents");
+		String thumbnail = request.getParameter("thumbnail");
 		
+	%>
 	
-	</script>
+	<tr>
+			<td><%=url%></td>
+			<td><%=title%></td>	
+			<td><%=contents%></td>
+			<td><%=thumbnail%></td>		
+	</tr>
+	
+	 <div class="buttons">
+  
+           <!-- <input type="button" onclick="location.href='form_agree.jsp'" value="회원가입"> --> 
+              <input type="button" onclick="./like" value="♡ 찜하기">
+            <!-- <input type="button"  onclick="check(form)" value="로그인" > -->
+            <input type="submit" value="구매하기" >
+      </div>
 	
 </body>
 </html>
