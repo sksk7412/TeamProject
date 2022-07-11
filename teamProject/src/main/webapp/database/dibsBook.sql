61df795d46277a00a7f9462849f1ebdfaaa98bb1
Administrator <Administrator@322-25>
2022-07-11 13:07:49 +0900

    usersDB, dibsBookDB

--- teamProject/src/main/webapp/database/dibsBook.sql
+++ teamProject/src/main/webapp/database/dibsBook.sql
@@ -5,9 +5,10 @@ create table dibsBook(
 	title varchar(500) not null,
     thumbnail varchar(500) not null,
     isbn varchar(20) primary key,
-    usercode int,
-    authors varchar(20),
-    createAt datetime
+    id int,
+    authors varchar(20) not null,
+    createAt datetime,
+    foreign key (id) references users(id)
 );
 
-select * from user;
+select * from dibsBook;
