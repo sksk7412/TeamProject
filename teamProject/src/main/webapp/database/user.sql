bfda2061fb4c5e993e49d9ccd138455922f729cf
Administrator <Administrator@322-25>
2022-07-11 12:19:23 +0900

    userDB_

--- teamProject/src/main/webapp/database/user.sql
+++ teamProject/src/main/webapp/database/user.sql
@@ -5,9 +5,9 @@ create table user(
 	id varchar(20) unique not null,
     pw varchar(20) not null,
     `name` varchar(20) not null,
-    usercode int primary key,
+    usercode smallint(8) primary key,
+    mobile varchar(13) not null,
     address varchar(500)
 );
-
 
 select * from user;
