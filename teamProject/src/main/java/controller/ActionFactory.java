8b41ee04bd8f6d6652b111fc22994f07604770fc
Administrator <Administrator@322-28>
2022-07-12 18:22:09 +0900

    7-12

--- teamProject/src/main/java/controller/ActionFactory.java
+++ teamProject/src/main/java/controller/ActionFactory.java
@@ -1,6 +1,8 @@
 package controller;
 
 import controller.action.JoinAction;
+import controller.action.LoginAction;
+import controller.action.LogoutAction;
 
 public class ActionFactory {
 	public ActionFactory() {}
@@ -10,11 +12,11 @@ public class ActionFactory {
 	}
 	
 	public Action getAction(String command) {
-		Action action =null;
+		Action action = null;
 		
 		if (command.equals("join")) action = new JoinAction();
-		else if(command.equals("login")) action = null;
-		else if(command.equals("logout")) action = null;
+		else if(command.equals("login")) action = new LoginAction();
+		else if(command.equals("logout")) action = new LogoutAction();
 		else if(command.equals("writeBoard")) action = null;
 		else if(command.equals("updateBoard")) action = null;
 		else if(command.equals("deletevBoard")) action = null;
