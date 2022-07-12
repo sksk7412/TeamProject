28069ecf537936fb303a51edf3d7db7ba6d0b646
Administrator <Administrator@322-25>
2022-07-12 19:02:20 +0900

    isbnDB

--- teamProject/src/main/webapp/database/myLibrary.sql
+++ teamProject/src/main/webapp/database/myLibrary.sql
@@ -10,6 +10,7 @@ create table myLibrary(
     foreign key (id) references users(id)
 );
 
-insert into myLibrary values(1,"0001","2022-07-11");
+insert into myLibrary values(1,"8990982707 9788990982704","2022-07-11");
+insert into myLibrary values(1,"1169180949 9791169180948","2022-07-11");
 
 select * from myLibrary;
