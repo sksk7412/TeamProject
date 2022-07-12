2194bc6cfc1661a10e4693711db3a8b600258f4b
Administrator <Administrator@322-25>
2022-07-12 13:11:38 +0900

    최최최종DB

--- teamProject/src/main/webapp/database/dibsBook.sql
+++ teamProject/src/main/webapp/database/dibsBook.sql
@@ -3,14 +3,14 @@ create database book;
 use book;
 
 create table dibsBook(
+	id int,
     isbn varchar(30) primary key,
-    id int,
     createAt datetime,
     foreign key (id) references users(id)
 );
   
-insert into dibsBook values("0001",1,"2022-07-12");
-insert into dibsBook values("0002",1,"2022-07-12");
-insert into dibsBook values("0003",1,"2022-07-12");
+insert into dibsBook values(1,"0001","2022-07-12");
+insert into dibsBook values(1,"0002","2022-07-12");
+insert into dibsBook values(1,"0003","2022-07-12");
 
 select * from dibsBook;
--- teamProject/src/main/webapp/database/myLibrary.sql
+++ teamProject/src/main/webapp/database/myLibrary.sql
@@ -3,11 +3,13 @@ create database book;
 use book;
 
 create table myLibrary(
+	id int,
     isbn varchar(30),
-    id int,
-    modifiedAt datetime, 
+    modifiedAt datetime,
     foreign key (isbn) references dibsBook(isbn),
     foreign key (id) references users(id)
-); 
+);
+
+insert into myLibrary values(1,"0001","2022-07-11");
 
 select * from myLibrary;
