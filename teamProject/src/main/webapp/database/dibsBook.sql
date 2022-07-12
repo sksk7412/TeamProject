67d78762790103883099a2b2963aca58de1f46fe
Administrator <Administrator@322-25>
2022-07-12 13:04:02 +0900

    db

--- teamProject/src/main/webapp/database/dibsBook.sql
+++ teamProject/src/main/webapp/database/dibsBook.sql
@@ -8,7 +8,7 @@ create table dibsBook(
     createAt datetime,
     foreign key (id) references users(id)
 );
- 
+  
 insert into dibsBook values("0001",1,"2022-07-12");
 insert into dibsBook values("0002",1,"2022-07-12");
 insert into dibsBook values("0003",1,"2022-07-12");
--- teamProject/src/main/webapp/database/myLibrary.sql
+++ teamProject/src/main/webapp/database/myLibrary.sql
@@ -5,7 +5,7 @@ use book;
 create table myLibrary(
     isbn varchar(30),
     id int,
-    modifiedAt datetime,
+    modifiedAt datetime, 
     foreign key (isbn) references dibsBook(isbn),
     foreign key (id) references users(id)
 ); 
