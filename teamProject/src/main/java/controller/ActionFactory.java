79e04aebec448bba6d4d4df3b9c110a93d626a13
Administrator <Administrator@322-26>
2022-07-14 18:17:51 +0900

    0714-189

---
+++ teamProject/src/main/java/controller/ActionFactory.java
@@ -0,0 +1,30 @@
+package controller;
+
+import controller.action.DibsAction;
+import controller.action.JoinAction;
+import controller.action.LoginAction;
+import controller.action.LogoutAction;
+import controller.action.WriteReviewAction;
+
+public class ActionFactory {
+	public ActionFactory() {}
+	private static ActionFactory instance = new ActionFactory();
+	public static ActionFactory getInstance() {
+		return instance;
+	}
+	
+	public Action getAction(String command) {
+		Action action = null;
+		
+		if (command.equals("join")) action = new JoinAction();
+		else if(command.equals("login")) action = new LoginAction();
+		else if(command.equals("logout")) action = new LogoutAction();
+		else if(command.equals("DibsBook")) action = new DibsAction();
+		else if(command.equals("writeBoard")) action = null;
+		else if(command.equals("writeReview")) action = new WriteReviewAction();
+		else if(command.equals("updateBoard")) action = null;
+		else if(command.equals("deleteBoard")) action = null;
+		
+		return action;
+	}
+}
---
+++ teamProject/src/main/java/teamProject/BoardDTO.java
@@ -0,0 +1,51 @@
+package teamProject;
+
+import java.sql.Timestamp;
+
+public class BoardDTO {
+	
+	private int log;
+	private int isbn;
+	private String contents;
+	private Timestamp createdAt;
+	
+	public BoardDTO(int log, int isbn, String contents, Timestamp createdAt) {
+		this.log=log;
+		this.isbn=isbn;
+		this.contents=contents;
+		this.createdAt=createdAt;
+	}
+
+	public int getLog() {
+		return log;
+	}
+
+	public void setLog(int log) {
+		this.log = log;
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public Timestamp getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Timestamp createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+}
