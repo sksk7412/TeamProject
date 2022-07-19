d5a920b3f0e978cd938ae4fe321d383040ce97b1
Administrator <Administrator@322-25>
2022-07-19 16:07:22 +0900

    04:07

--- teamProject/src/main/java/controller/ActionFactory.java
+++ teamProject/src/main/java/controller/ActionFactory.java
@@ -4,6 +4,7 @@ import controller.action.DeleteDibsAction;
 import controller.action.DeleteLisAction;
 import controller.action.DibsAction;
 import controller.action.JoinAction;
+import controller.action.LibraryAction;
 import controller.action.LoginAction;
 import controller.action.LogoutAction;
 import controller.action.UpdateAction;
@@ -27,6 +28,7 @@ public class ActionFactory {
 		else if(command.equals("Dibsbook")) action = new DibsAction();
 		else if(command.equals("updateUser")) action = new UpdateAction();
 		else if(command.equals("deleteDibs")) action = new DeleteDibsAction();
+		else if(command.equals("addLibrary")) action = new LibraryAction();
 		else if(command.equals("deleteLis")) action = new DeleteLisAction();
 		else if(command.equals("updateBoard")) action = null;
 		else if(command.equals("deleteBoard")) action = null;
